608cebb4a406a8438246b63ed4b3e51ca8487e8a
Felix Kopp <sandtler@sandtler.club>
2020-10-12 12:45:13 +0200

    Fix another pile of stupid bugs

--- include/ardix/list.h
+++ include/ardix/list.h
@@ -25,50 +25,49 @@ struct list_head {
 	container_of(ptr, type, member)
 
 #define list_next_entry(ptr, member) \
-	container_of((ptr)->member->next, typeof(*(ptr)), member)
+	container_of((ptr)->member.next, typeof(*(ptr)), member)
 
 #define list_prev_entry(ptr, member) \
-	container_of((ptr)->member->prev, typeof(*(ptr)), member)
+	container_of((ptr)->member.prev, typeof(*(ptr)), member)
 
 #define list_first_entry(head, type, member) \
 	container_of((head)->next, type, member)
 
-#define list_last_entry(head, type, memner) \
+#define list_last_entry(head, type, member) \
 	container_of((head)->prev, type, member)
 
 #define list_is_empty(head) ((head)->next == head)
 
 #define list_for_each(head, cursor)		\
 	for (cursor = (head)->next;		\
-	     cursor != head;			\
+	     cursor != (head);			\
 	     cursor = (cursor)->next)
 
 #define list_for_each_reverse(head, cursor)	\
 	for (cursor = (head)->prev;		\
-	     cursor != head;			\
+	     cursor != (head);			\
 	     cursor = (cursor)->prev)
 
 #define list_for_each_safe(head, cursor, tmp)		\
 	for (cursor = (head)->next, tmp = cursor;	\
-	     tmp != head;				\
+	     tmp != (head);				\
 	     tmp = (tmp)->next, cursor = tmp)
 
 #define list_for_each_entry(head, cursor, member)				\
 	for (cursor = list_first_entry(head, typeof(*(cursor)), member);	\
-	     &(cursor)->member != head;						\
+	     &(cursor)->member != (head);					\
 	     cursor = list_next_entry(cursor, member))
 
 #define list_for_each_entry_reverse(head, cursor, member)			\
 	for (cursor = list_last_entry(head, typeof(*(cursor)), member);		\
-	     &(cursor)->member != head;						\
+	     &(cursor)->member != (head);					\
 	     cursor = list_prev_entry(cursor, member))
 
 #define list_for_each_entry_safe(head, cursor, tmp, member)			\
 	for (cursor = list_first_entry(head, typeof(*(cursor)), member),	\
-	     tmp = list_next_entry(cursor, member);				\
-	     tmp != head;							\
-	     cursor = tmp,							\
-	     tmp = list_next_entry(tmp, member))
+		 tmp = list_next_entry(cursor, member);				\
+	     tmp != (head);							\
+	     cursor = tmp, tmp = list_next_entry(tmp, member))
 
 void list_insert(struct list_head *head, struct list_head *new)
 {
