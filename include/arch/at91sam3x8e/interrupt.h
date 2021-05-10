f0dc3a34332818ef2583247e908874efa10694af
Felix <owo@fef.moe>
2021-05-10 16:19:38 +0200

    kiss capitalism goodbye for good
    The CNPL will make sure that absolutely no corporation is going to get
    anywhere near Ardix -- exactly as it should be.  Fuck you, corporations!

--- include/arch/at91sam3x8e/interrupt.h
+++ include/arch/at91sam3x8e/interrupt.h
@@ -1,4 +1,3 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later */
 /* See the end of this file for copyright, license, and warranty information. */
 
 #pragma once
@@ -173,16 +172,11 @@ void arch_irq_invoke(enum irqno irqno);
  * This file is part of Ardix.
  * Copyright (c) 2020, 2021 Felix Kopp <owo@fef.moe>.
  *
- * Ardix is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Ardix is non-violent software: you may only use, redistribute,
+ * and/or modify it under the terms of the CNPLv6+ as found in
+ * the LICENSE file in the source code root directory or at
+ * <https://git.pixie.town/thufie/CNPL>.
  *
- * Ardix is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Ardix comes with ABSOLUTELY NO WARRANTY, to the extent
+ * permitted by applicable law.  See the CNPLv6+ for details.
  */
--- kernel/ringbuf.c
+++ kernel/ringbuf.c
@@ -1,4 +1,3 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later */
 /* See the end of this file for copyright, license, and warranty information. */
 
 /* Simple ring buffer implementation. */
@@ -6,6 +5,7 @@
 #include <ardix/malloc.h>
 #include <ardix/ringbuf.h>
 #include <ardix/types.h>
+
 #include <stddef.h>
 
 struct ringbuf *ringbuf_create(size_t size)
@@ -61,16 +61,11 @@ size_t ringbuf_write(struct ringbuf *buf, const void *src, size_t len)
  * This file is part of Ardix.
  * Copyright (c) 2020, 2021 Felix Kopp <owo@fef.moe>.
  *
- * Ardix is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Ardix is non-violent software: you may only use, redistribute,
+ * and/or modify it under the terms of the CNPLv6+ as found in
+ * the LICENSE file in the source code root directory or at
+ * <https://git.pixie.town/thufie/CNPL>.
  *
- * Ardix is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Ardix comes with ABSOLUTELY NO WARRANTY, to the extent
+ * permitted by applicable law.  See the CNPLv6+ for details.
  */
