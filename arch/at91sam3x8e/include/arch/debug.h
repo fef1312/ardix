8422ab0d56df8134fb4ba6e30c78569e08bbcdb7
fef <owo@fef.moe>
2021-08-01 22:53:47 +0200

    debug: add breakpoint macro

---
+++ arch/at91sam3x8e/include/arch/debug.h
@@ -0,0 +1,24 @@
+/* See the end of this file for copyright, license, and warranty information. */
+
+#pragma once
+
+#include <config.h>
+
+#ifdef DEBUG
+#define __breakpoint __asm__ volatile("\tbkpt\n" ::: )
+#else
+#define __breakpoint
+#endif
+
+/*
+ * This file is part of Ardix.
+ * Copyright (c) 2020, 2021 Felix Kopp <owo@fef.moe>.
+ *
+ * Ardix is non-violent software: you may only use, redistribute,
+ * and/or modify it under the terms of the CNPLv6+ as found in
+ * the LICENSE file in the source code root directory or at
+ * <https://git.pixie.town/thufie/CNPL>.
+ *
+ * Ardix comes with ABSOLUTELY NO WARRANTY, to the extent
+ * permitted by applicable law.  See the CNPLv6+ for details.
+ */
