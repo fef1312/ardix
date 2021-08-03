e291a51d2fd66687a02b02186834737d2fc49943
fef <owo@fef.moe>
2021-08-03 22:44:28 +0200

    kevent: add event system

---
+++ include/ardix/kevent.h
@@ -0,0 +1,104 @@
+/* See the end of this file for copyright, license, and warranty information. */
+
+#pragma once
+
+#include <ardix/device.h>
+#include <ardix/kent.h>
+#include <ardix/list.h>
+
+#include <toolchain.h>
+
+/**
+ * @defgroup event Event Subsystem
+ *
+ * Events are a simple message broadcast mechanism to notify the kernel about
+ * changes in hardware in an asynchronous way.  Messages are enqueued from irq
+ * context and then processed in the scheduling routine before choosing a new
+ * task to run.  Using this mechanism, tasks can be suspended to I/O sleep and
+ * then woken up again when the underlying device driver has received an irq
+ * indicating the device is ready for new data.
+ *
+ * @{
+ */
+
+enum kevent_kind {
+	KEVENT_FS,
+
+	KEVENT_KIND_COUNT,
+};
+
+/**
+ * @brief kevent base structure.
+ * Every type of kevent has its own structure embedding this one.
+ */
+struct kevent {
+	struct kent kent;
+	struct list_head link;	/**< list node for the event queue */
+	enum kevent_kind kind;
+};
+
+/** @brief Initialize the kevent subsystem. */
+void kevents_init(void);
+
+/** @brief Process all kevents in the queue.  Called by the scheduler. */
+void kevents_process(void);
+
+/**
+ * @brief Dispatch an event and transfer its ownership to the kevent subsystem.
+ * The event's kent must be initialized to have the device driver the event
+ * comes from as its parent, and an appropriate destroy callback that is safe
+ * to be invoked from non irq context.
+ *
+ * @param event kevent to dispatch
+ */
+void kevent_dispatch(struct kevent *event);
+
+/**
+ * @brief Add an event listener to the end of the listener queue.
+ * The callback will be invoked for every event that is dispatched and matches
+ * the event kind.  If its return value is nonzero, event handler processing
+ * stops after that callback.  This is useful if you know for sure that you are
+ * the only one interested in the event, for example when waiting for I/O.
+ *
+ * @param kind Kind of kevent to listen for
+ * @param cb Callback that will be invoked for every kevent that is dispatched
+ * @param extra An optional extra pointer that will be passed to the callback
+ * @returns 0 on success, or a negtive error number on failure
+ */
+int kevent_add_listener(enum kevent_kind kind,
+			int (*cb)(struct kevent *event, void *extra),
+			void *extra);
+
+/**
+ * @brief Remove an event listener.
+ *
+ * @param kind Kind that was passed to `kevent_add_listener()`
+ * @param cb Callback that was passed to `kevent_add_listener()`
+ */
+void kevent_remove_listener(enum kevent_kind kind,
+			    int (*cb)(struct kevent *event, void *extra));
+
+__always_inline void kevent_get(struct kevent *event)
+{
+	kent_get(&event->kent);
+}
+
+__always_inline void kevent_put(struct kevent *event)
+{
+	kent_put(&event->kent);
+}
+
+/** @} */
+
+/*
+ * This file is part of Ardix.
+ * Copyright (c) 2021 Felix Kopp <owo@fef.moe>.
+ *
+ * Ardix is non-violent software: you may only use, redistribute,
+ * and/or modify it under the terms of the CNPLv6+ as found in
+ * the LICENSE file in the source code root directory or at
+ * <https://git.pixie.town/thufie/CNPL>.
+ *
+ * Ardix comes with ABSOLUTELY NO WARRANTY, to the extent
+ * permitted by applicable law.  See the CNPLv6+ for details.
+ */
---
+++ kernel/kevent.c
@@ -0,0 +1,195 @@
+/* See the end of this file for copyright, license, and warranty information. */
+
+/**
+ * @file kevent implementation.
+ *
+ * This implementation is kinda fucked, because it basically gambles on system
+ * ticks
+ */
+
+#include <ardix/atom.h>
+#include <ardix/malloc.h>
+#include <ardix/mutex.h>
+#include <ardix/kent.h>
+#include <ardix/kevent.h>
+#include <ardix/list.h>
+
+#include <errno.h>
+#include <stddef.h>
+
+/* event listeners indexed by event type */
+static struct list_head kev_listeners[KEVENT_KIND_COUNT];
+static MUTEX(kev_listeners_lock);
+
+struct kevent_queue {
+	struct list_head list;	/* -> kevent_listener::link */
+	struct mutex lock;
+};
+
+/* event queues indexed by event type */
+static struct kevent_queue kev_queues[KEVENT_KIND_COUNT];
+
+/**
+ * irqs cannot sleep or block, so we can only *try* to claim the lock on
+ * an event queue when dispatching an event.  If this fails (which shouldn't
+ * happen only rarely), we store the event in this (unsorted) pile and sort them
+ * out before processing the event queue.  We technically need a lock for this
+ * queue as well, but that's okay because we don't
+ */
+LIST_HEAD(kev_cache);
+MUTEX(kev_cache_lock);
+
+struct kevent_listener {
+	struct list_head link;	/* -> kevent_queue::list */
+	void *extra;
+	int (*cb)(struct kevent *event, void *extra);
+};
+
+void kevents_init(void)
+{
+	for (int i = 0; i < KEVENT_KIND_COUNT; i++) {
+		list_init(&kev_listeners[i]);
+		list_init(&kev_queues[i].list);
+		mutex_init(&kev_queues[i].lock);
+	}
+}
+
+/* called from scheduler context only */
+static inline void process_single_queue(struct kevent_queue *queue, struct list_head *listeners)
+{
+	struct kevent *event, *tmp;
+
+	/*
+	 * This method is only invoked from scheduler context which has higher
+	 * exception priority than all irqs, so if claiming the lock on this
+	 * list fails it means we interrupted the irq.  The current strategy is
+	 * to just abort and try again during the next system tick.
+	 */
+	if (mutex_trylock(&queue->lock) == 0) {
+		list_for_each_entry_safe(&queue->list, event, tmp, link) {
+			struct kevent_listener *listener;
+
+			list_for_each_entry(listeners, listener, link) {
+				int cb_ret = listener->cb(event, listener->extra);
+				if (cb_ret != 0)
+					break;
+			}
+
+			list_delete(&event->link);
+			kevent_put(event);
+		}
+
+		mutex_unlock(&queue->lock);
+	}
+}
+
+/* called from scheduler context only */
+void kevents_process(void)
+{
+	/*
+	 * if this fails it means the scheduling interrupt happened while
+	 * processing an irq, just ignore the cache and try again next time
+	 * if that is the case
+	 */
+	if (mutex_trylock(&kev_cache_lock) == 0) {
+		struct kevent *cursor, *tmp;
+		list_for_each_entry_safe(&kev_cache, cursor, tmp, link) {
+			list_insert(&kev_queues[cursor->kind].list, &cursor->link);
+			list_delete(&cursor->link);
+		}
+
+		mutex_unlock(&kev_cache_lock);
+	}
+
+	for (int i = 0; i < KEVENT_KIND_COUNT; i++)
+		process_single_queue(&kev_queues[i], &kev_listeners[i]);
+}
+
+/* called from irq context only */
+void kevent_dispatch(struct kevent *event)
+{
+	struct kevent_queue *queue = &kev_queues[event->kind];
+
+	if (mutex_trylock(&queue->lock) == 0) {
+		list_insert(&queue->list, &event->link);
+		mutex_unlock(&queue->lock);
+	} else {
+		/*
+		 * We shouldn't ever be able to get here because irqs don't interrupt
+		 * each other and if we get interrupted by the scheduler it doesn't
+		 * matter because kevents_process() always releases its lock before
+		 * returning again.  If it still happens for whatever stupid reason,
+		 * we insert the event in a temporary unsorted cache that is then
+		 * ordered by the scheduler.
+		 */
+		if (mutex_trylock(&kev_cache_lock) == 0) {
+			list_insert(&kev_cache, &event->link);
+			mutex_unlock(&kev_cache_lock);
+		} else {
+			/*
+			 * If we ever make it to here, something *extremely* stupid
+			 * has happened: we couldn't get the lock on the queue (which
+			 * shouldn't ever happen in the first place), *and* the cache
+			 * is locked as well.  We will have to assume we got suspended
+			 * at some point in these if branches, and just try getting
+			 * the lock on the original queue again.  If that fails as
+			 * well, we just give up and discard the event all together.
+			 *
+			 * TODO: This solution is of course far from ideal and has to
+			 *       be refactored at some point before the first stable
+			 *       release.  We'll just deal with that later(TM).
+			 */
+			if (mutex_trylock(&queue->lock) == 0) {
+				list_insert(&queue->list, &event->link);
+				mutex_unlock(&queue->lock);
+			} else {
+				kevent_put(event);
+			}
+		}
+	}
+}
+
+int kevent_add_listener(enum kevent_kind kind, int (*cb)(struct kevent *, void *), void *extra)
+{
+	struct kevent_listener *listener = malloc(sizeof(*listener));
+	if (listener == NULL)
+		return -ENOMEM;
+
+	listener->cb = cb;
+	listener->extra = extra;
+
+	mutex_lock(&kev_listeners_lock);
+	list_insert(&kev_listeners[kind], &listener->link);
+	mutex_unlock(&kev_listeners_lock);
+
+	return 0;
+}
+
+void kevent_remove_listener(enum kevent_kind kind, int (*cb)(struct kevent *, void *))
+{
+	struct kevent_listener *listener, *tmp;
+
+	list_for_each_entry_safe(&kev_listeners[kind], listener, tmp, link) {
+		if (listener->cb == cb) {
+			mutex_lock(&kev_listeners_lock);
+			list_delete(&listener->link);
+			mutex_unlock(&kev_listeners_lock);
+
+			free(listener);
+			break;
+		}
+	}
+}
+
+/*
+ * This file is part of Ardix.
+ * Copyright (c) 2021 Felix Kopp <owo@fef.moe>.
+ *
+ * Ardix is non-violent software: you may only use, redistribute,
+ * and/or modify it under the terms of the CNPLv6+ as found in
+ * the LICENSE file in the source code root directory or at
+ * <https://git.pixie.town/thufie/CNPL>.
+ *
+ * Ardix comes with ABSOLUTELY NO WARRANTY, to the extent
+ * permitted by applicable law.  See the CNPLv6+ for details.
+ */
